bafb32ea9c02d989f7d730c92049d0a0fbfe731e
stefan.apostolovski97 <stefan.apostolovski97@gmail.com>
2023-07-21 01:58:23 +0200

    fix next export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
-    domains: ['cloudflare-ipfs.com']
+    domains: ['cloudflare-ipfs.com'],
+    loader: 'akamai',
+    path: '/'
   }
 }
 
